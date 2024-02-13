b15946303979b4e98796ae755d11af4be8b36cb8
Flavia S. Briglia <fbrig87@gmail.com>
2024-02-12 21:38:36 -0300

    crea componente Search

--- src/screens/ItemListCategories.jsx
+++ src/screens/ItemListCategories.jsx
@@ -1,12 +1,14 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { colors } from "../global/colors.js";
 import ProductItem from "../components/ProductItem";
+import Search from "../components/Search.jsx";
 import products from "../data/products.json";
 
 const ItemListCategories = () => {
   return (
     <View>
       <Text style={styles.titleSection}>ItemListCategories</Text>
+      <Search />
       <FlatList
         data={products}
         renderItem={({ item  }) => <ProductItem product={item} />} // Renderiza este componente por cada elemento en el array - Desestructura "item"
@@ -19,6 +21,7 @@ const ItemListCategories = () => {
 export default ItemListCategories;
 
 const styles = StyleSheet.create({
+  
   titleSection: {
     fontSize: 30,
     backgroundColor: colors.secondary,
