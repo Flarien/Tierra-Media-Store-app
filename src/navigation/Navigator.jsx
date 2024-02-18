df43e2286e0bc9f14a04d41880c2677bd4fe5f61
Flavia S. Briglia <fbrig87@gmail.com>
2024-02-18 15:39:32 -0300

    crea componente Navigator

---
+++ src/navigation/Navigator.jsx
@@ -0,0 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet } from "react-native";
+import Home from "../screens/Home";
+import ItemDetail from "../screens/ItemDetail";
+import ItemListCategories from "../screens/ItemListCategories";
+
+const Navigator = () => {
+  const Stack = createNativeStackNavigator();
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ItemDetail" component={ItemDetail} />
+        <Stack.Screen
+          name="ItemListCategories"
+          component={ItemListCategories}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigator;
+
+const styles = StyleSheet.create({});
